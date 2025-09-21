9622d8ff591d0ff0971bac1588aa3caaa9e9ba21
anschek <anschek@yandex.ru>
2025-09-21 12:39:14 +0300

    build: create basis of project

---
+++ include/task.h
@@ -0,0 +1,23 @@
+//
+// Created by anschek on 21.09.2025.
+//
+
+#ifndef TASK_MANAGER_ON_POINTERS_TASK_H
+#define TASK_MANAGER_ON_POINTERS_TASK_H
+
+#include <memory>
+
+enum Priority { LOW, MEDIUM, HIGH };
+struct Task {
+    void (*func)();
+    Priority priority;
+};
+
+extern std::unique_ptr<Task[]> g_tasks;
+extern size_t g_task_count;
+
+void add_task(Task task);
+void execute_tasks_in_order();
+void execute_tasks_by_priority();
+
+#endif //TASK_MANAGER_ON_POINTERS_TASK_H
